798212cfc2f37e9d4e0c3cf3a7f3165e2a87e65f
Omnia369 <thegoldenlight108@gmail.com>
2025-11-27 15:44:16 -0600

    Update pages/toolkits/[id].tsx to use shared Layout component wrapping existing content

--- pages/toolkits/[id].tsx
+++ pages/toolkits/[id].tsx
@@ -1,48 +1,31 @@
-import { GetStaticProps, GetStaticPaths } from 'next';
-import Head from 'next/head';
-import { getAllToolkitIds, getToolkitData } from '../../lib/toolkits';
-import { ParsedUrlQuery } from 'querystring';
-import Layout from '../../components/layout';
+import { getToolkitData, getToolkitIds } from '../../lib/toolkits';
+import Layout from '../../components/Layout';
 
-interface IParams extends ParsedUrlQuery {
-  id: string;
-}
-
-export default function Toolkit({
-  toolkitData,
-}: {
-  toolkitData: {
-    title: string;
-    contentHtml: string;
-  };
-}) {
-  return (
-    <Layout>
-      <Head>
-        <title>{toolkitData.title}</title>
-      </Head>
-      <article className='prose lg:prose-xl'>
-        <h1>{toolkitData.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: toolkitData.contentHtml }} />
-      </article>
-    </Layout>
-  );
-}
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = getAllToolkitIds();
+export async function getStaticPaths() {
+  const paths = getToolkitIds();
   return {
     paths,
     fallback: false,
   };
-};
+}
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const { id } = context.params as IParams;
-  const toolkitData = await getToolkitData(id);
+export async function getStaticProps({ params }) {
+  const toolkitData = await getToolkitData(params.id);
   return {
     props: {
       toolkitData,
     },
   };
+}
+
+const ToolkitPage = ({ toolkitData }) => {
+  return (
+    <Layout>
+      <div className='bg-white p-8 rounded-lg shadow'>
+        <div className='prose max-w-none' dangerouslySetInnerHTML={{ __html: toolkitData.contentHtml }} />
+      </div>
+    </Layout>
+  );
 };
+
+export default ToolkitPage;
